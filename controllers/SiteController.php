68fc02a52af3f872cf61768d499c85ac186a03f4
mrozk <mrozk2012@gmail.com>
2016-10-05 13:23:04 +0300

    Clean up

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -28,42 +28,10 @@ class SiteController extends Controller
         ];
 
         return $behaviors;
-        /*return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];*/
+
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
+
 
     /**
      * Displays homepage.
@@ -75,37 +43,6 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
-    /**
-     * Login action.
-     *
-     * @return string
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Logout action.
-     *
-     * @return string
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
 
 
     private function getUserValues(){
@@ -208,34 +145,5 @@ class SiteController extends Controller
         ];
     }
 
-    /**
-     * Displays contact page.
-     *
-     * @return string
-     */
-    public function actionContact()
-    {
-        $model = new Dictionary();
-        print_r($model->getTestQueue());
-        print_r($model->getVariants(12));
-        /*$model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
 
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);*/
-    }
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
-    }
 }
---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div ng-view></div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="/assets/public/jquery/jquery.min.js"></script>
+<script src="/assets/public/angular/angular.min.js"></script>
+<script src="/assets/public/angular/angular-route.min.js"></script>
+<script src="/assets/public/bootstrap/js/bootstrap.min.js"></script>
+<script src="/assets/app/js/app.js"></script>
+<script src="/assets/app/js/main/main.js"></script>
+<script src="/assets/app/js/test/test.js"></script>
